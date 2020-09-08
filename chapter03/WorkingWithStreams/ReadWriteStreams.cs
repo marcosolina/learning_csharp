ef235c6756b2e69820cb778f4f362a1bab557833
Marco <marcosolina@gmail.com>
2020-09-08 07:46:02 +0200

    finishing chapter 3

--- chapter03/WorkingWithStreams/ReadWriteStreams.cs
+++ chapter03/WorkingWithStreams/ReadWriteStreams.cs
@@ -10,6 +10,14 @@ namespace WorkingWithStreams
             StreamWriter streamWriter = new StreamWriter("test.txt");
             streamWriter.WriteLine("Hello Marco");
             streamWriter.Close();
+
+            StreamReader streamReader = new StreamReader("test.txt");
+            string line = streamReader.ReadLine();
+            while(streamReader.EndOfStream == false)
+            {
+                Console.WriteLine(line);
+                streamReader.Close();
+            }
         }
     }
 }
